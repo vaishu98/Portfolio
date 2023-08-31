c9759e134083a27279aee18de595f74127785335
Vaishnavi Ch <aruna.avvs@gmail.com>
2023-08-31 00:23:15 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sign from './MyComponents/Sign';
+
+function App() {
+  return (
+    <div className="App">
+      <Sign></Sign>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponents/Sign.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Sign.css"
+
+export default function Sign() {
+    return (
+        <>
+            <link href="https://fonts.cdnfonts.com/css/la-sonnambula" rel="stylesheet" />
+            <style>
+                @import url('https://fonts.cdnfonts.com/css/la-sonnambula');
+            </style>
+            <div className='V'>
+                <vdown></vdown>
+                <vdown2></vdown2>
+                <vdown3></vdown3>
+                <vup></vup>
+                <vup2></vup2>
+                <vup3></vup3>
+                <c></c>
+                <c3></c3>
+                <c2></c2>
+            </div>
+            <line></line>
+            <line2></line2>
+            <div class='at-container'>
+                <div class='at-item' >Vaishnavi Chilakamarthi</div>
+            </div>
+        </>
+    )
+}
